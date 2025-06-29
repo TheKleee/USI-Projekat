2fc6aded7dacfaab29a7dc2f7583bf0f3ab23f27
TheKleee <mk.kleee@gmail.com>
2025-06-29 11:34:29 +0200

    Init
    Inicijalni push na github za USI projekat

---
+++ MarkoKosticIT6922/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using MarkoKosticIT6922.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarkoKosticIT6922.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MarkoKosticIT6922/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarkoKosticIT6922.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
